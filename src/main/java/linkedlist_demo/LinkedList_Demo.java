5103df0ed62d11edd35058e83780934fd39804b9
offic <offic@DESKTOP-GLF4UNU>
2024-07-05 12:25:26 +0600

    Using the "remove" method on linked_list.

--- src/main/java/linkedlist_demo/LinkedList_Demo.java
+++ src/main/java/linkedlist_demo/LinkedList_Demo.java
@@ -8,11 +8,24 @@ public class LinkedList_Demo {
         LinkedList<String> linkedList = new LinkedList<>();
         linkedList.add("Afghanistan");
         linkedList.add("Pakistan");
-        linkedList.add("Bangladesh");
         linkedList.add("India");
+        linkedList.add("UK");
+        linkedList.add("USA");
+        linkedList.add("UAE");
+        linkedList.add("Sri Lanka");
+        linkedList.add("India");
+        linkedList.add("Maldives");
         linkedList.add("Nepel");
+        
         linkedList.add(2,"Saudi Arabia"); //একটি element-কে কততম size-এ দেখানো হবে, তা নির্দিষ্ট করে দেওয়া যায়।।
-
+        linkedList.addFirst("Bangladesh");//এর মানে এই index কে সবার উপরে দেখাও
+        linkedList.addLast("Japan");      //এর মানে এই index কে সবার নিচে দেখাও     
+        
+        linkedList.remove("Japan");       //এর মানে এই নির্দিষ্ট index কে remove কর
+        linkedList.remove(8);             //এর মানে এতো number index কে remove কর
+        linkedList.removeFirst();         //এর মানে প্রথম index কে remove কর
+        linkedList.removeLast();          //এর মানে শেষ index কে remove কর        
+                
         //'for each loop' use করে result দেখানো হয়েছে।।
         for (String country : linkedList) {
             System.out.println(country);
