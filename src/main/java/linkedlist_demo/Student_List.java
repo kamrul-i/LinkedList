458b04776d12d15d9028a00ffcf70085fb94550d
offic <offic@DESKTOP-GLF4UNU>
2024-07-05 16:25:06 +0600

    Linked_List Demo result customize.

--- src/main/java/linkedlist_demo/Student_List.java
+++ src/main/java/linkedlist_demo/Student_List.java
@@ -1,42 +1,54 @@
 package linkedlist_demo;
-
 import java.util.LinkedList;
-
 public class Student_List {
 
     public static void main(String[] args) {
         //এখানে ২টি class use করা হয়েছে।। এই class থেকে অন্য class কে call করা হয়েছে।।
-
-        LinkedList<Students> studentList = new LinkedList<>();
-
+        //--------------------------------------------------------
+        LinkedList<Students> studentList1 = new LinkedList<>();
+        
         Students student1 = new Students(101, "Abu Ishaq");
         Students student2 = new Students(102, "Abu Raihan");
         Students student3 = new Students(103, "Tanisa Begum");
         Students student4 = new Students(104, "Sharmin Begum");
         Students student5 = new Students(105, "Abdullah Arman");
+        
+        studentList1.add(student1);
+        studentList1.add(student2);
+        studentList1.add(student3);
+        studentList1.add(student4);
+        studentList1.add(student5);
+        
+        for (Students s : studentList1) {
+            System.out.println("ID        : " + s.id);
+            System.out.println("Name      : " + s.name);
+            System.out.println("ClassName : " + Students.className1);
+            System.out.printf("\n");
+        }
+        //--------------------------------------------------------
+        
+        
+        //--------------------------------------------------------
+        LinkedList<Students> studentList2 = new LinkedList<>();
+        
         Students student6 = new Students(106, "Abu Saleh");
         Students student7 = new Students(107, "Dilruba Begum");
         Students student8 = new Students(108, "Azhari");
         Students student9 = new Students(109, "Taha Adnan");
         Students student10 = new Students(110, "Nasima Begum");
-
-        studentList.add(student1);
-        studentList.add(student2);
-        studentList.add(student3);
-        studentList.add(student4);
-        studentList.add(student5);
-        studentList.add(student6);
-        studentList.add(student7);
-        studentList.add(student8);
-        studentList.add(student9);
-        studentList.add(student10);
-
-        for (Students s : studentList) {
+        
+        studentList2.add(student6);
+        studentList2.add(student7);
+        studentList2.add(student8);
+        studentList2.add(student9);
+        studentList2.add(student10);        
+        
+        for (Students s : studentList2) {
             System.out.println("ID        : " + s.id);
             System.out.println("Name      : " + s.name);
-            System.out.println("ClassName : " + Students.className);
+            System.out.println("ClassName : " + Students.className2);
             System.out.printf("\n");
-
         }
+        //--------------------------------------------------------
     }
 }
--- src/main/java/linkedlist_demo/Students.java
+++ src/main/java/linkedlist_demo/Students.java
@@ -1,16 +1,15 @@
 package linkedlist_demo;
-
 public class Students {
     //এখানে ২টি class use করা হয়েছে।। এই class-এ variable declare করে, অন্য class থেকে তাকে call করা হয়েছে।।
 
     String name;
     int id;
-    static String className = "twelve";
-    //static ব্যবহার করে সবাইকে same Class-এ ধরা হয়েছে।।
+    static String className1 = "Eight";
+    static String className2 = "Nine";
+    //static ব্যবহার করে সবাইকে ২টি Class-এ ধরা হয়েছে।।
 
     Students(int id, String name) {
         this.id = id;
         this.name = name;
     }
-
 }
