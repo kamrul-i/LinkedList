fb1efd0f1e35eb5cdcd397918cafce7e6d6c8af3
offic <offic@DESKTOP-GLF4UNU>
2024-07-05 10:58:36 +0600

    Introduction to Linked_List.

---
+++ src/main/java/linkedlist/LinkedList.java
@@ -0,0 +1,8 @@
+package linkedlist;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP.\"");
+    }
+}
